ed02c299d2ab2c5ab9b5764e8326ae0e2e7e2bf1
janika <jkiss12@gmail.com>
2022-07-17 22:50:21 +0100

    Added just Hi messages

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+# Mini projects from youtube
+# Please check his account https://www.youtube.com/watch?v=DLn3jOsNRVE
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# This is the simplest command line game that I could came up with based on the
+# tutorial ... so Enjoy it
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Hi stranger!")
+print("Welcome to the Quiz Game!")
